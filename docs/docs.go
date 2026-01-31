5fa8666c1f9732161d90f55341a199cbd8b396d3
Void Panda <hamzankechil@gmail.com>
2026-01-31 21:24:24 +0800

    Fix: Swagger Docs host

--- docs/docs.go
+++ docs/docs.go
@@ -420,7 +420,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Kasir API",
